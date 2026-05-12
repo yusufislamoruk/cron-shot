6411b138e29cb7a2af0fd30fe25b3337de0197a9
yusufislamoruk <yusufislamoruk@gmail.com>
2026-05-12 16:58:31 +0300

    Feat: scheduling util file.

---
+++ src/utils/scheduling.ts
@@ -0,0 +1,19 @@
+import { ScheduledJob } from "../types";
+
+export function calculateNextRunAt(frequency: ScheduledJob['frequency'],timeOfDay: ScheduledJob['time_of_day']): Date {
+    const [hours, minutes] = timeOfDay.split(':').map(Number);
+    const now = new Date();
+    
+    const next = new Date(now);
+    next.setHours(hours,minutes,0,0);
+
+    if(next <= now ) {
+        switch (frequency) {
+            case 'daily': next.setDate(next.getDate() + 1); break;
+            case 'weekly': next.setDate(next.getDate() + 7); break;
+            case 'monthly': next.setDate(next.getDate() + 30); break;
+        }
+    }
+
+    return next;
+}

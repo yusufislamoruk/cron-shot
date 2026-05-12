49db3607fc1898b1827a61e86d65aa51a456beef
yusufislamoruk <yusufislamoruk@gmail.com>
2026-05-12 17:48:01 +0300

    Feat: scheduled added.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import { PORT } from "./config/env";
+import { runScheduler } from "./services/scheduler";
 import app from "./app";
 
-app.listen(PORT, () => {
+const  start = async () => {
+    const SCHEDULE_INTERVAL_MS = 60 * 1000;
+
+    setInterval(async () => {
+    await runScheduler();
+    }, SCHEDULE_INTERVAL_MS);
+
+
+    await runScheduler();
+
+    app.listen(PORT, () => {
     console.log(`CronShot backend running on port ${PORT}`);
-});
+  })
+};
+
+start();
